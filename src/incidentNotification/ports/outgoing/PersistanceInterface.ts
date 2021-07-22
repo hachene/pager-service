75464e442860528e2ef6326181e35099d5b4c493
Juan Martinez Palazon <jmp.juan@gmail.com>
2021-07-22 20:55:23 +0200

    :sparkles: Add method getAlertByMonitoredServiceId to PersistanceInterface port

--- src/incidentNotification/ports/outgoing/PersistanceInterface.ts
+++ src/incidentNotification/ports/outgoing/PersistanceInterface.ts
@@ -1,6 +1,8 @@
+import { Alert } from '../../core/models/Alert'
 import { MonitoredService } from '../../core/models/MonitoredService'
 
 export interface PersistanceInterface {
   markMonitoredServiceAsUnhealthy(id: MonitoredService['id']): void
   getMonitoredServiceById(id: MonitoredService['id']): MonitoredService
+  getAlertByMonitoredServiceId(monitoredServiceId: MonitoredService['id']): Alert
 }
