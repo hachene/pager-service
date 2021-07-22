ad6b979c6f2d59c2fc5f53a30b950a791858c3ca
Juan Martinez Palazon <jmp.juan@gmail.com>
2021-07-22 18:08:53 +0200

    :sparkles: Add Monitored Service model

---
+++ src/incidentNotification/core/models/MonitoredService.ts
@@ -0,0 +1,13 @@
+export class MonitoredService {
+  public id: number
+  public status: MonitoredServiceStatus
+
+  constructor({ id, status }: Partial<MonitoredService>) {
+    this.id = id
+    this.status = status
+  }
+}
+
+export enum MonitoredServiceStatus {
+  healthy = 'healthy',
+}
