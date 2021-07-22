bbd450b1dc72b7ca492a28353f7da723ef2b5ee4
Juan Martinez Palazon <jmp.juan@gmail.com>
2021-07-22 18:56:00 +0200

    :sparkles: WIP: Add retrieval escalation policy functionality to NotifyUnhealthyService

--- src/incidentNotification/core/NotifyUnhealthyService.test.ts
+++ src/incidentNotification/core/NotifyUnhealthyService.test.ts
@@ -1,6 +1,10 @@
+import { EscalationPolicyServiceInterface } from '../ports/outgoing/EscalationPolicyServiceInterface'
 import { PersistanceInterface } from '../ports/outgoing/PersistanceInterface'
 import { Alert } from './models/Alert'
+import { EscalationPolicy } from './models/EscalationPolicy'
 import { MonitoredService, MonitoredServiceStatus } from './models/MonitoredService'
+import { EmailTarget } from './models/Target/EmailTarget'
+import { SmsTarget } from './models/Target/SmsTarget'
 import { NotifyUnhealthyService } from './NotifyUnhealthyService'
 
 describe('NotifyUnhealthyService', () => {
@@ -8,15 +12,38 @@ describe('NotifyUnhealthyService', () => {
   let monitoredService: MonitoredService
   let alert: Alert
   let markMonitoredServiceAsUnhealthyMock: jest.Mock<any, any>
+  let getEscalationPolicyByServiceIdMock: (id: MonitoredService['id']) => EscalationPolicy
 
-  beforeAll(() => {
+  beforeEach(() => {
     markMonitoredServiceAsUnhealthyMock = jest.fn()
     const persistanceAdapterMock: PersistanceInterface = {
       markMonitoredServiceAsUnhealthy: markMonitoredServiceAsUnhealthyMock,
     }
 
-    subject = new NotifyUnhealthyService(persistanceAdapterMock)
+    const targetsFirstLevel = [
+      new EmailTarget({ emailAddress: 'raquel@aircall.com' }),
+      new EmailTarget({ emailAddress: 'juan@aircall.com' }),
+    ]
+    const targetsLastLevel = [
+      new SmsTarget({ phoneNumber: '+ 33 1 40 00 00 00' }),
+      new SmsTarget({ phoneNumber: '+ 33 1 40 00 00 00' }),
+    ]
+
+    const returnedEscalationPolicy = new EscalationPolicy({
+      monitoredServiceId: 1,
+      levels: [targetsFirstLevel, targetsLastLevel],
+    })
+
+    getEscalationPolicyByServiceIdMock = jest.fn(() => returnedEscalationPolicy)
+
+    const escalationPolicyServiceAdapterMock: EscalationPolicyServiceInterface = {
+      getEscalationPolicyByServiceId: getEscalationPolicyByServiceIdMock,
+    }
+
+    subject = new NotifyUnhealthyService(persistanceAdapterMock, escalationPolicyServiceAdapterMock)
   })
+
+  afterEach(() => jest.clearAllMocks())
 
   describe('given a Monitored Service in an Healthy State', () => {
     describe('when the Pager receives an Alert related to this Monitored Service', () => {
@@ -36,6 +63,13 @@ describe('NotifyUnhealthyService', () => {
 
         expect(markMonitoredServiceAsUnhealthyMock).toHaveBeenCalledTimes(1)
         expect(markMonitoredServiceAsUnhealthyMock).toHaveBeenCalledWith(alert.monitoredServiceId)
+      })
+
+      it('the Pager notifies all targets of the first level of the escalation policy', () => {
+        subject.perform(alert)
+
+        expect(getEscalationPolicyByServiceIdMock).toHaveBeenCalledTimes(1)
+        expect(getEscalationPolicyByServiceIdMock).toHaveBeenCalledWith(alert.monitoredServiceId)
       })
     })
   })
--- src/incidentNotification/core/NotifyUnhealthyService.ts
+++ src/incidentNotification/core/NotifyUnhealthyService.ts
@@ -1,15 +1,19 @@
+import { EscalationPolicyServiceInterface } from '../ports/outgoing/EscalationPolicyServiceInterface'
 import { PersistanceInterface } from '../ports/outgoing/PersistanceInterface'
 import { Alert } from './models/Alert'
 
 export class NotifyUnhealthyService {
   private persistance: PersistanceInterface
+  private escalationPolicyService: EscalationPolicyServiceInterface
 
-  constructor(persistanceInterface: PersistanceInterface) {
+  constructor(persistanceInterface: PersistanceInterface, escalationPolicyService: EscalationPolicyServiceInterface) {
     this.persistance = persistanceInterface
+    this.escalationPolicyService = escalationPolicyService
   }
 
   public perform({ monitoredServiceId }: NotifyUnhealthyServiceParams): void {
     this.persistance.markMonitoredServiceAsUnhealthy(monitoredServiceId)
+    const escalationPolicy = this.escalationPolicyService.getEscalationPolicyByServiceId(monitoredServiceId)
   }
 }
 
