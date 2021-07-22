7747b65a190939b859962aad04faa580d6daf58d
Juan Martinez Palazon <jmp.juan@gmail.com>
2021-07-22 18:41:31 +0200

    :sparkles: Add EscalationPolicy model

---
+++ src/incidentNotification/core/models/EscalationPolicy.ts
@@ -0,0 +1,12 @@
+import { MonitoredService } from './MonitoredService'
+import { Target } from './Target/Target'
+
+export class EscalationPolicy {
+  public monitoredServiceId: MonitoredService['id']
+  public levels: Target[][]
+
+  constructor({ monitoredServiceId, levels }: Partial<EscalationPolicy>) {
+    this.monitoredServiceId = monitoredServiceId
+    this.levels = levels
+  }
+}
