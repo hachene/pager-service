58165424e81224048da207e027648c8df5e51adb
Juan Martinez Palazon <jmp.juan@gmail.com>
2021-07-23 08:55:58 +0200

    :sparkles: Modify Alert model to have isAcknowledge and areLastLevelTargetsNotified properties

--- src/incidentNotification/core/models/Alert.ts
+++ src/incidentNotification/core/models/Alert.ts
@@ -1,7 +1,11 @@
 export class Alert {
   public monitoredServiceId: number
+  public isAcknowledge: boolean
+  public areLastLevelTargetsNotified: boolean
 
-  constructor({ monitoredServiceId }: Partial<Alert>) {
+  constructor({ monitoredServiceId, isAcknowledge: acknowledged, areLastLevelTargetsNotified }: Partial<Alert>) {
     this.monitoredServiceId = monitoredServiceId
+    this.isAcknowledge = acknowledged
+    this.areLastLevelTargetsNotified = areLastLevelTargetsNotified
   }
 }
