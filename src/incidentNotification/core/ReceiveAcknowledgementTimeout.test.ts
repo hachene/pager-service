90bbd838c48e9221b9e467d5b19ecb8bd7edb5e2
Juan Martinez Palazon <jmp.juan@gmail.com>
2021-07-26 18:58:29 +0200

    :white_check_mark: Add test cases to ensure ReceiveAcknowledgementTimeout does not contact targets nor set delay when the alert is acknowledged

--- src/incidentNotification/core/ReceiveAcknowledgementTimeout.test.ts
+++ src/incidentNotification/core/ReceiveAcknowledgementTimeout.test.ts
@@ -1,9 +1,11 @@
 import {
   buildEscalationPolicyServiceAdapterMock,
+  buildMailServiceAdapterMock,
   buildPersistanceInterfaceAdapterMock,
   buildSmsServiceAdapterMock,
   buildTimerServiceAdapterMock,
 } from '../../testUtils/mocks'
+import { MailServiceInterface } from '../ports/outgoing/MailServiceInterface'
 import { PersistanceInterface } from '../ports/outgoing/PersistanceInterface'
 import { SmsServiceInterface } from '../ports/outgoing/SmsServiceInterface'
 import { TimerServiceInterface } from '../ports/outgoing/TimerServiceInterface'
@@ -17,49 +19,52 @@ describe('ReceiveAcknowledgementTimeout', () => {
   let subject: ReceiveAcknowledgementTimeout
 
   let smsServiceAdapterMock: SmsServiceInterface
+  let mailServiceAdapterMock: MailServiceInterface
   let persistanceServiceAdapterMock: PersistanceInterface
   let timerServiceAdapterMock: TimerServiceInterface
 
   describe('given a Monitored Service in an Unhealthy State', () => {
-    beforeEach(() => {
-      const targetsFirstLevel = [
-        new EmailTarget({ emailAddress: 'raquel@aircall.com' }),
-        new EmailTarget({ emailAddress: 'juan@aircall.com' }),
-      ]
-      const targetsLastLevel = [
-        new SmsTarget({ phoneNumber: '+ 33 1 40 00 00 00' }),
-        new SmsTarget({ phoneNumber: '+ 33 1 40 00 00 00' }),
-      ]
-      const returnedEscalationPolicy = new EscalationPolicy({
-        monitoredServiceId: 1,
-        levels: [targetsFirstLevel, targetsLastLevel],
-      })
-
-      const escalationPolicyServiceAdapterMock = buildEscalationPolicyServiceAdapterMock({
-        getEscalationPolicyByServiceId: jest.fn(() => returnedEscalationPolicy),
-      })
-
-      smsServiceAdapterMock = buildSmsServiceAdapterMock()
-
-      const returnedAlert = new Alert({
-        monitoredServiceId: 1,
-        areLastLevelTargetsNotified: false,
-        isAcknowledge: false,
-      })
-      persistanceServiceAdapterMock = buildPersistanceInterfaceAdapterMock({
-        getAlertByMonitoredServiceId: jest.fn(() => returnedAlert),
-      })
-
-      timerServiceAdapterMock = buildTimerServiceAdapterMock()
-
-      subject = new ReceiveAcknowledgementTimeout(
-        escalationPolicyServiceAdapterMock,
-        smsServiceAdapterMock,
-        persistanceServiceAdapterMock,
-        timerServiceAdapterMock,
-      )
-    })
     describe('and the corresponding Alert is not Acknowledged', () => {
+      beforeEach(() => {
+        const targetsFirstLevel = [
+          new EmailTarget({ emailAddress: 'raquel@aircall.com' }),
+          new EmailTarget({ emailAddress: 'juan@aircall.com' }),
+        ]
+        const targetsLastLevel = [
+          new SmsTarget({ phoneNumber: '+ 33 1 40 00 00 00' }),
+          new SmsTarget({ phoneNumber: '+ 33 1 40 00 00 00' }),
+        ]
+        const returnedEscalationPolicy = new EscalationPolicy({
+          monitoredServiceId: 1,
+          levels: [targetsFirstLevel, targetsLastLevel],
+        })
+
+        const escalationPolicyServiceAdapterMock = buildEscalationPolicyServiceAdapterMock({
+          getEscalationPolicyByServiceId: jest.fn(() => returnedEscalationPolicy),
+        })
+
+        smsServiceAdapterMock = buildSmsServiceAdapterMock()
+        mailServiceAdapterMock = buildMailServiceAdapterMock()
+
+        const returnedAlert = new Alert({
+          monitoredServiceId: 1,
+          areLastLevelTargetsNotified: false,
+          isAcknowledge: false,
+        })
+        persistanceServiceAdapterMock = buildPersistanceInterfaceAdapterMock({
+          getAlertByMonitoredServiceId: jest.fn(() => returnedAlert),
+        })
+
+        timerServiceAdapterMock = buildTimerServiceAdapterMock()
+
+        subject = new ReceiveAcknowledgementTimeout(
+          escalationPolicyServiceAdapterMock,
+          smsServiceAdapterMock,
+          persistanceServiceAdapterMock,
+          timerServiceAdapterMock,
+        )
+      })
+
       describe('and the last level has not been notified', () => {
         it('the Pager notifies all targets of the next level of the escalation policy when receive a timeout', () => {
           subject.perform({ monitoredServiceId: 1 })
@@ -76,6 +81,55 @@ describe('ReceiveAcknowledgementTimeout', () => {
             monitoredServiceId: 1,
           })
         })
+      })
+    })
+
+    describe('and the corresponding Alert is Acknowledged', () => {
+      beforeEach(() => {
+        const targetsFirstLevel = [new EmailTarget({ emailAddress: 'raquel@aircall.com' })]
+        const targetsLastLevel = [new SmsTarget({ phoneNumber: '+ 33 1 40 00 00 00' })]
+
+        const returnedEscalationPolicy = new EscalationPolicy({
+          monitoredServiceId: 1,
+          levels: [targetsFirstLevel, targetsLastLevel],
+        })
+
+        const escalationPolicyServiceAdapterMock = buildEscalationPolicyServiceAdapterMock({
+          getEscalationPolicyByServiceId: jest.fn(() => returnedEscalationPolicy),
+        })
+
+        smsServiceAdapterMock = buildSmsServiceAdapterMock()
+        mailServiceAdapterMock = buildMailServiceAdapterMock()
+
+        const returnedAlert = new Alert({
+          monitoredServiceId: 1,
+          areLastLevelTargetsNotified: false,
+          isAcknowledge: true,
+        })
+        persistanceServiceAdapterMock = buildPersistanceInterfaceAdapterMock({
+          getAlertByMonitoredServiceId: jest.fn(() => returnedAlert),
+        })
+
+        timerServiceAdapterMock = buildTimerServiceAdapterMock()
+
+        subject = new ReceiveAcknowledgementTimeout(
+          escalationPolicyServiceAdapterMock,
+          smsServiceAdapterMock,
+          persistanceServiceAdapterMock,
+          timerServiceAdapterMock,
+        )
+      })
+      it('the Pager does not notify any Target', () => {
+        subject.perform({ monitoredServiceId: 1 })
+
+        expect(smsServiceAdapterMock.sendAlert).not.toHaveBeenCalled()
+        expect(mailServiceAdapterMock.sendAlert).not.toHaveBeenCalled()
+      })
+
+      it('the Pager does not set any acknowledgement delay', () => {
+        subject.perform({ monitoredServiceId: 1 })
+
+        expect(timerServiceAdapterMock.setTimerForAlert).not.toHaveBeenCalled()
       })
     })
   })
